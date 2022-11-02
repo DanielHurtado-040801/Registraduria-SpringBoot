f2e9807b6e4b9c4fcf67c1340528b9fa304b6c1e
Daniel Hurtado <danielestebanhd.kb24@gmail.com>
2022-11-02 02:39:42 -0500

    Sprint-2 / REG-58

--- src/main/java/com/gestion_usuarios/usuarios/Models/Partido.java
+++ src/main/java/com/gestion_usuarios/usuarios/Models/Partido.java
@@ -1,5 +1,43 @@
 package com.gestion_usuarios.usuarios.Models;
 
+import org.springframework.data.annotation.Id;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+@Document()
 public class Partido {
+    @Id
+    private String id;
+    private String name;
+    private String lema;
+
     
+    public Partido(String name, String lema) {
+        this.name = name;
+        this.lema = lema;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLema() {
+        return lema;
+    }
+
+    public void setLema(String lema) {
+        this.lema = lema;
+    }
 }
